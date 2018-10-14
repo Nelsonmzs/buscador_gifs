53b0d6d5ec4fc21a529575322af78cd4a2931e21
Nelsonmzs <nelson30_8@hotmail.com>
2018-10-13 23:56:39 -0300

    - Cria método para retornar os melhores gifs, ou então, o tema escolhido pelo usuário

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,8 +11,37 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  
+  String _search;
+  int _offset = 0;
+    
+  Future<Map> _getGifs() async {
+  
+    http.Response response;
+    
+    if (_search == null) {
+      response = await http.get("https://api.giphy.com/v1/gifs/trending?api_key=uDI3Lf7m7gJy9Ez0Ykfw6yLBMo2babne&limit=20&rating=G");
+    } else {
+      response = await http.get("https://api.giphy.com/v1/gifs/search?api_key=uDI3Lf7m7gJy9Ez0Ykfw6yLBMo2babne&q=$_search&limit=20&$_offset=75&rating=G&lang=en");
+    }
+    return json.decode(response.body);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _getGifs().then((map) {
+      print(map);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+      ),
+    );
   }
 }
