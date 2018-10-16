ad0aeb91a2de1d6e75b7f1c556b85c6ebd77f9b5
Nelsonmzs <nelson30_8@hotmail.com>
2018-10-15 21:20:29 -0300

    - Monta grid para apresentar os Gifs

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -35,7 +35,6 @@ class _HomePageState extends State<HomePage> {
     super.initState();
 
     _getGifs().then((map) {
-      print(map);
     });
   }
 
@@ -62,8 +61,56 @@ class _HomePageState extends State<HomePage> {
               textAlign: TextAlign.center,
             ),
           ),
+          Expanded(
+           child: FutureBuilder(
+             future: _getGifs(),
+             builder: (context, snapshot) {
+               switch(snapshot.connectionState) {
+                 case ConnectionState.waiting:
+                 case ConnectionState.none:
+                   return Container(
+                   width: 200.0,
+                     height: 200.00,
+                     alignment: Alignment.center,
+                     child: CircularProgressIndicator(
+                       valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                       strokeWidth: 5.0,
+                     ),
+                   );
+                 default:
+                   if (snapshot.hasError) {
+                     return Container();
+                   } else {
+                     return _createGifTable(context, snapshot);
+                   }
+               }
+           },
+           ),
+          ),
         ],
       ),
     );
+  }
+
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
+
+    return GridView.builder(
+      padding: EdgeInsets.all(10.0),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+          crossAxisSpacing: 10.0,
+          mainAxisSpacing: 10.0,
+        ),
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            child: Image.network(snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+            height: 300.0,
+              fit: BoxFit.cover,
+            ),
+          );
+        }
+    );
+
   }
 }
